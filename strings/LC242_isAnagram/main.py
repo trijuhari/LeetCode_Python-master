a8b206e1c5ddca507a1ba87a828db941ae5c19e3
hirazone <trijuhari0@gmail.com>
2023-07-29 21:25:50 +0700

    LC242 done

--- strings/LC242_isAnagram/main.py
+++ strings/LC242_isAnagram/main.py
@@ -1,2 +1,11 @@
 class Solution:
     def isAnagram(self, s, t):
+        s= "".join(sorted(s))
+        t= "".join(sorted(t))
+        if  len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
