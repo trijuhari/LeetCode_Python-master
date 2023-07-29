9d2ecf57e7bfbb5e168db61b5e19cb679f0b17a1
hirazone <trijuhari0@gmail.com>
2023-07-29 19:03:52 +0700

    LC3

--- strings/LC3_lengthOfLongestSubstring/main.py
+++ strings/LC3_lengthOfLongestSubstring/main.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_2:
     def lengthOfLongestSubstring(self, s):
         length =0
         l = 0
@@ -11,3 +11,20 @@ class Solution:
                 length = max(length, i -l+1)
             seen[char] = i
         return length
+
+class Solution:
+    def lengthOfLongestSubstring(self,s):
+        start = 0
+        end =  0
+        substring =""
+        length =0 
+        while  end <len(s):
+            while start < end and s[end] in substring:
+                start +=1
+                substring = s[start:end]
+
+            substring += s[end]
+            length = max(length, len(substring))
+            end +=1 
+        return length
+
