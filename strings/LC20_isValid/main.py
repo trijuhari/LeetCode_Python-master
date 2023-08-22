1632e3eb429027609a4f3b1bc1ea88da5576531c
hirazone <trijuhari0@gmail.com>
2023-08-22 11:59:08 +0700

    Bismillah

--- strings/LC20_isValid/main.py
+++ strings/LC20_isValid/main.py
@@ -1,2 +1,13 @@
 class Solution:
     def isValid(self, s):
+        stack = []
+        pair_hash_map = {"(": ")", "{":"}","[":"]"}
+        for char in   s:
+            if   char in pair_hash_map:
+                stack.append(char)
+                print(stack)
+            elif len(stack) == 0 or pair_hash_map[stack.pop()] != char:
+                return False
+        return  len(stack) == 0
+# a = Solution()
+# print(a.isValid('([{}])'))
